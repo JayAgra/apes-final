a91d1a5b991c5922f6823f0f6fab7e7dd2110254
JayAgra <jay@jayagra.com>
2025-05-16 13:55:12 -0700

    cooking

--- js/index.js
+++ js/index.js
@@ -1,5 +1,73 @@
-Array.from(document.querySelectorAll(".navLinks button")).forEach((element, index) => {
+var navLinks = Array.from(document.querySelectorAll(".navLinks button"));
+
+document.body.onload = () => {
+    document.loadContent(0)
+}
+
+navLinks.forEach((element, index) => {
     element.addEventListener("click", () => {
-        console.log("Tab selected: " + Array.from(["Nitrogen", "Carbon", "Phosphorous", "Water"])[index])
+        console.log("Tab selected: " + Array.from(["c", "h", "n", "p"])[index])
+        navLinks.forEach((element) => {
+            element.classList.remove("navLinkButtonSelected");
+        });
+        navLinks[index].classList.add("navLinkButtonSelected");
+        document.loadContent(index)
     });
 });
+
+document.loadContent = (index) => {
+    console.log("Loading content with index " + index)
+    var stepElements = [];
+    
+};
+
+const CYCLES = [
+    {
+        title: "Carbon",
+        base_image: "",
+        layer_images: [""],
+        steps: [
+            {
+                title: "",
+                brief: "",
+                html: "",
+            },
+        ],
+    },
+    {
+        title: "Water",
+        base_image: "",
+        layer_images: [""],
+        steps: [
+            {
+                title: "",
+                brief: "",
+                html: "",
+            },
+        ],
+    },
+    {
+        title: "Nitrogen",
+        base_image: "",
+        layer_images: [""],
+        steps: [
+            {
+                title: "",
+                brief: "",
+                html: "",
+            },
+        ],
+    },
+    {
+        title: "Phosphorous",
+        base_image: "",
+        layer_images: [""],
+        steps: [
+            {
+                title: "",
+                brief: "",
+                html: "",
+            },
+        ],
+    },
+];
